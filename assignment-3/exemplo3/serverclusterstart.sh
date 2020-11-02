e87b24bc6c583269b36af3d5d461515176d1cd59
José Legatheaux Martins <jose.legatheaux@fct.unl.pt>
2020-11-02 19:00:40 +0000

    Update serverclusterstart.sh

--- assignment-3/exemplo3/serverclusterstart.sh
+++ assignment-3/exemplo3/serverclusterstart.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+kill $(ps ax |grep "java -jar HttpTrickyServer.jar [0-9]*" |grep -v "grep"|awk '{print $1}')
+sleep 1
 echo "HttpTrickyServer 0"
 java -jar HttpTrickyServer.jar 8080 &
 echo "HttpTrickyServer 1"
