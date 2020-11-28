c173fd0e915b6e62427922304142a3f465fa59d6
José Legatheaux Martins <jose.legatheaux@fct.unl.pt>
2020-11-28 20:29:45 +0000

    Add files via upload

---
+++ assignment-5/DVRoutingTableEntry.java
@@ -0,0 +1,29 @@
+
+public class DVRoutingTableEntry {
+		private int dest;
+		private int iface;
+		private int metric;
+		private int last_update;
+
+		public DVRoutingTableEntry (int d, int i, int m, int t)
+		{
+			dest = d;
+			iface = i;
+			metric = m;
+			last_update = t;
+		}
+		
+		public int getDestination() { return dest; } 
+		public void setDestination(int d) {dest = d;}
+		public int getInterface() { return iface; }
+		public void setInterface(int i) {iface = i;}
+		public int getMetric() { return metric;}
+		public void setMetric(int m) {metric = m;} 
+		public int getTime() {return last_update;}
+		public void setTime(int t) {last_update = t;}
+
+		public String toString() 
+		{
+			return "d "+dest+" i "+iface+" m "+metric+" lu "+last_update;
+		}
+}
